c62de0b4fa160eb1b786a1df7a030483a7ef97f9
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-04 17:32:46 +0000

    Updated selenium nevigate

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
 
@@ -14,8 +14,33 @@ public class BasicNavigation {
 
         driver.get("https:tesla.com");
 
+        // stop code execution 3 seconds
+        Thread.sleep(3000);
 
-                        }
+        // use selenium to nevigate back
+        driver.navigate().back();
+
+        // stop code execution 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to nevigate forward
+        driver.navigate().forward();
+
+        // stop code execution 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to nevigate refresh
+        driver.navigate().refresh();
+
+        // use nevigate().to() method
+        driver.navigate().to("https:www.apple.com");
+
+        // get the title of the page
+        //driver.getTitle();
+        System.out.println(driver.getTitle());
+
+
+    }
 
     }
 
