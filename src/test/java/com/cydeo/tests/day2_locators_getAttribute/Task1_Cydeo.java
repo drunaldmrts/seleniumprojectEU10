b7c73fc74edffdc7a91324e1e583ab357709dd25
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-05 00:52:12 +0000

    Task 1 Cydeo

--- src/test/java/com/cydeo/tests/day2_locators_getAttribute/Task1_Cydeo.java
+++ src/test/java/com/cydeo/tests/day2_locators_getAttribute/Task1_Cydeo.java
@@ -7,14 +7,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Task1_Cydeo {
     public static void main(String[] args) {
 
+        // Open chrome browser
         WebDriverManager.chromedriver().setup();
-
         WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
 
+        // Go to https://practice.cydeo.com
         // driver.get("https://practice.cydeo.com");
         driver.navigate().to("https://practice.cydeo.com");
 
-
+        //Verify URL contains
         String expectedURL="cydeo";
         String actualURL= driver.getCurrentUrl();
 
@@ -24,6 +26,7 @@ public class Task1_Cydeo {
             System.out.println("FAILED!");
         }
 
+        // Verify title contains
         String expectedTitle="Practice";
         String actualTitle= driver.getTitle();
 
@@ -33,6 +36,7 @@ public class Task1_Cydeo {
             System.out.println("FAILED!");
         }
 
+        driver.close();
 
 
 
