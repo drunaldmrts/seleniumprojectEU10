171ffb05445a04b3e2014ce7480515b9c8547fb3
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-05 10:29:26 +0000

    Day 3 Task 1

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task1_Locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task1_Locators_getText.java
@@ -1,17 +1,18 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.Utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task1_Locators_getText {
 
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
+        //WebDriverManager.chromedriver().setup();
+        //WebDriver driver= new ChromeDriver();
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+
         driver.manage().window().maximize();
 
         //Go to: https://login1.nextbasecrm.com/
