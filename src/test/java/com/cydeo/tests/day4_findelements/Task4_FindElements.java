dacd952ad10b5f5a53682b94acc4eaade967a63d
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-06 08:44:55 +0000

    Day 4 Task 4 find elements

---
+++ src/test/java/com/cydeo/tests/day4_findelements/Task4_FindElements.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.day4_findelements;
+
+import com.cydeo.Utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Task4_FindElements {
+    public static void main(String[] args) {
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate all the links in the page.
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
+        //4- Print out the number of the links on the page.
+        System.out.println("allLinks.size() = " + allLinks.size());
+
+        //5- Print out the texts of the links.
+        for (WebElement each : allLinks) {
+            System.out.println("Test of link: "+each.getText());
+        }
+
+        //6- Print out the HREF attribute values of the links
+    }
+}
