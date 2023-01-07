1709721f548ab7add44bfceafc969df507a58292
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-07 23:29:25 +0000

    Day 6 Task 5 and Task 6

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/Task5_SelectState.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/Task5_SelectState.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -38,5 +39,39 @@ public class Task5_SelectState {
         String actualOption=stateDropdownOption.getFirstSelectedOption().getText();
 
         Assert.assertEquals(actualOption,expectedOption);
+
+        }
+    @Test
+    public void stateDropdown_task6(){
+        //Select “December 1st, 1923” and verify it is selected.
+        Select yearDropdown=new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown=new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown=new Select(driver.findElement(By.xpath("//select[@id='day']")));
+
+        //Select year using  : visible text
+        yearDropdown.selectByVisibleText("1924");
+
+        //Select month using   : value attribute
+        monthDropdown.selectByValue("11");
+
+        //Select day using : index number
+        dayDropdown.selectByIndex(0);
+
+        String expectedYear="1924";
+        String expectedMonth="December";
+        String expectedDay="1";
+
+        String actualYear=yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth=monthDropdown.getFirstSelectedOption().getText();
+        String actualDay=dayDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualYear,expectedYear);
+        Assert.assertEquals(actualMonth,expectedMonth);
+        Assert.assertEquals(actualDay,expectedDay);
+
+    }
+    @AfterMethod
+    public void tearDropdown(){
+        driver.close();
     }
 }
