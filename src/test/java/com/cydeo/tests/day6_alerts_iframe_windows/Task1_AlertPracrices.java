a1720fdf2203be0e1f904e697cb9ebe1ca9583fb
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-08 11:28:16 +0000

    Day 6 Task 2 Alert Practice

--- src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/Task1_AlertPracrices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/Task1_AlertPracrices.java
@@ -38,10 +38,27 @@ WebDriver driver;
         //5. Verify “You successfully clicked an alert” text is displayed.
         WebElement alertText= driver.findElement(By.xpath("(//p[@id='result'])"));
 
+        Assert.assertTrue(alertText.isDisplayed(),"Result text is NOT displayed");
+
         String expectedText="You successfully clicked an alert";
         String actualText=alertText.getText();
 
         Assert.assertEquals(actualText,expectedText);
-        //Assert.assertTrue(alertText.isDisplayed(),"Result text is NOT displayed");
+    }
+    @Test
+    public void alertTask_Task2(){
+        //Click to “Click for JS Confirm” button
+        WebElement confirmButton= driver.findElement(By.xpath("//button[.='Click for JS Confirm']"));
+
+        confirmButton.click();
+
+        //4. Click to OK button from the alert
+        Alert alert= driver.switchTo().alert();
+        alert.accept();
+
+        //5. Verify “You clicked: Ok” text is displayed.
+        WebElement resultText= driver.findElement(By.xpath("//p[.='You clicked: Ok']"));
+
+        Assert.assertTrue(resultText.isDisplayed());
     }
 }
