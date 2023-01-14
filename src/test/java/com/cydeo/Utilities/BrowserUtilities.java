634fe8b382633123dfae31c63b1a4a25951d2708
Unal Demirtas <drunaldmrts@outlook.com>
2023-01-14 21:32:41 +0000

    Browser utilies switch windows and verify method

--- src/test/java/com/cydeo/Utilities/BrowserUtilities.java
+++ src/test/java/com/cydeo/Utilities/BrowserUtilities.java
@@ -1,16 +1,44 @@
 package com.cydeo.Utilities;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+/*
+In this class only general utility methods that are not releated to some specific page
+ */
 public class BrowserUtilities {
 
     /*
     This method will accept int (in second) and execute Thread.sleep for given duration
      */
-    public void sleep(int second){
+    public static void sleep(int second){
+
+        second*=1000;
 
         try{
             Thread.sleep(second);
         }catch (InterruptedException e){
 
         }
+    }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle){
+
+        Set<String> allWindowHandles=driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL = " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+
+        }
+        String actualTitle= driver.getTitle();
+
     }
 }
