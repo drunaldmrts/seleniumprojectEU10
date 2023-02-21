ffc3fb870376ce6a2a1b8c0f237926b03cd73516
Unal Demirtas <drunaldmrts@outlook.com>
2023-02-21 12:56:00 +0000

    Day 12 POM

--- src/test/java/com/cydeo/tests/day12_PageObjectModel/POM_Practices.java
+++ src/test/java/com/cydeo/tests/day12_PageObjectModel/POM_Practices.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class POM_Practices {
 
+    LibraryLoginPage libraryLoginPage;
     @Test
     public void require_field_error_message_test(){
         //Open a chrome browser
@@ -15,12 +16,48 @@ public class POM_Practices {
 
         //3- Do not enter any information
         //4- Click to “Sign in” button
-        LibraryLoginPage libraryLoginPage=new LibraryLoginPage;
+        LibraryLoginPage libraryLoginPage= new LibraryLoginPage();
 
         libraryLoginPage.signInButton.click();
 
         //5- Verify expected error is displayed:
         Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
 
+        Driver.closeDriver();
+    }
+    @Test
+    public void invalid_email_format_error_message_test() {
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+
+        //3- Enter invalid email format
+        libraryLoginPage=new LibraryLoginPage();
+
+        libraryLoginPage.inputUsername.sendKeys("somethingwrong");
+        libraryLoginPage.signInButton.click();
+
+        //4- Verify expected error is displayed:
+        Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
+    }
+    @Test
+    public void library_negative_login_test(){
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+        libraryLoginPage=new LibraryLoginPage();
+
+        //3- Enter incorrect username or incorrect password
+        libraryLoginPage.inputUsername.sendKeys("wrongemail@email.com");
+        libraryLoginPage.inputPassword.sendKeys("wrongpassword");
+
+        libraryLoginPage.signInButton.click();
+
+        //4- Verify title expected error is displayed:
+        Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
     }
 }
